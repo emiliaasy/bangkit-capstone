d1a80a46610677b79f80dae862151c2e25ecde88
Emilia Syafitri <emilia_sy@apps.ipb.ac.id>
2022-05-27 18:43:48 +0700

    Main Menu

--- app/src/main/java/com/emiliaasy/androidintermediate/capstone/MainActivity.kt
+++ app/src/main/java/com/emiliaasy/androidintermediate/capstone/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.emiliaasy.androidintermediate.capstone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emiliaasy.androidintermediate.capstone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.newButton.setOnClickListener {
+            startActivity(Intent(this, CategoryActivity::class.java))
+        }
     }
 }
