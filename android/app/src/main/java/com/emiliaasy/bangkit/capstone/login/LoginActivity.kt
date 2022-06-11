e431d7278acf0896bff755883177e257fcdc75cb
emiliaasy <emilia_sy@apps.ipb.ac.id>
2022-06-11 14:49:19 +0700

    Login Page

--- android/app/src/main/java/com/emiliaasy/bangkit/capstone/login/LoginActivity.kt
+++ android/app/src/main/java/com/emiliaasy/bangkit/capstone/login/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.emiliaasy.bangkit.capstone.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.emiliaasy.bangkit.capstone.R
+import com.emiliaasy.bangkit.capstone.databinding.ActivityLoginBinding
+import com.emiliaasy.bangkit.capstone.dialog.ExitDialogActivity
+import com.emiliaasy.bangkit.capstone.signup.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        actionView()
+    }
+
+    private fun actionView() {
+        //
     }
 }
