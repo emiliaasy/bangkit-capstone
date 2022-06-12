014a9ecc74794f0f00902d3f16dc5fa41ff7c115
Hafiz <hafiz.rfikri@gmail.com>
2022-06-12 19:08:21 +0700

    update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore({
   projectId: 'jovial-rex-351311',
-  keyFilename: '\jovial-rex-351311-1cee0c462786.json',
+  keyFilename: './jovial-rex-351311-1cee0c462786.json',
 });
 
 module.exports = db;
