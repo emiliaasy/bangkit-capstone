d034fc60b69702025f038a86d9b3d9cf34b3c937
emiliaasy <emilia_sy@apps.ipb.ac.id>
2022-06-12 09:24:35 +0700

    result page done

--- android/app/src/main/java/com/emiliaasy/bangkit/capstone/result/IncorrectResultActivity.kt
+++ android/app/src/main/java/com/emiliaasy/bangkit/capstone/result/IncorrectResultActivity.kt
@@ -4,15 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.emiliaasy.bangkit.capstone.ExplanationActivity
-import com.emiliaasy.bangkit.capstone.databinding.ActivityCorrectResultBinding
+import com.emiliaasy.bangkit.capstone.databinding.ActivityIncorrectResultBinding
 import com.emiliaasy.bangkit.capstone.dialog.PauseDialogActivity
 
-class CorrectResultActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityCorrectResultBinding
+class IncorrectResultActivity  : AppCompatActivity() {
+    private lateinit var binding: ActivityIncorrectResultBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityCorrectResultBinding.inflate(layoutInflater)
+        binding = ActivityIncorrectResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.hide()
