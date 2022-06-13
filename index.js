f9207184900eace0d9662c11be7b62741318c4d1
Hafiz <hafiz.rfikri@gmail.com>
2022-06-13 11:02:47 +0700

    API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./route')
 const app = express();
-const PORT = 5000;
+const PORT = 8080;
 
 app.use(express.json());
 app.use('/',router)
