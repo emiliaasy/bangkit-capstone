b4c4c3c30bf9d263e5aeabf7ec805a22dfcc37ca
HafizFikri <hafiz.rfikri@gmail.com>
2022-06-15 20:04:19 +0700

    Delete API directory

--- API/db.js
+++
@@ -1,8 +0,0 @@
-const Firestore = require('@google-cloud/firestore');
-
-const db = new Firestore({
-  projectId: 'jovial-rex-351311',
-  keyFilename: './jovial-rex-351311-1cee0c462786.json',
-});
-
-module.exports = db;
